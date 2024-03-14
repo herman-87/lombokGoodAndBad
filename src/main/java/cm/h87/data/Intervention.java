7bbb151a25f20f4ad7a3ea1dd09cb0ad2eb5313a
essoungou.wonga <essoungou.wonga@lao-sarl.cm>
2024-03-14 12:08:34 +0100

    first commit

---
+++ src/main/java/cm/h87/data/Intervention.java
@@ -0,0 +1,10 @@
+package cm.h87.data;
+
+import java.time.LocalDate;
+
+public class Intervention {
+    private LocalDate interventionDate;
+    private Long mileage;
+    private String label;
+    private double money;
+}
---
+++ src/main/java/cm/h87/data/Vehicle.java
@@ -0,0 +1,10 @@
+package cm.h87.data;
+
+import java.time.LocalDate;
+
+public class Vehicle {
+    private Long engineNumber;
+    private Long chassisNumber;
+    private Long matriculationNumber;
+    private LocalDate dateOfCirculation;
+}
